36eb238b43f519ff84071b4f8868583883976eb1
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-03 14:46:33 -0700

    completed 1.4

--- chapter-1-arrays-and-strings/1.4-palindrome-permutation.py
+++ chapter-1-arrays-and-strings/1.4-palindrome-permutation.py
@@ -6,10 +6,35 @@ def is_palindrome_permutation(strng):
         >>> is_palindrome_permutation("Tact Coa")
         True
 
+        >>> is_palindrome_permutation("ababada")
+        True
+
+        >>> is_palindrome_permutation("  tac ocats")
+        False
+
     """
+    strng_dict = {}
+    for char in strng:
+        if char != " ":
+            if char.lower() in strng_dict:
+                strng_dict[char.lower()] += 1
+            else:
+                strng_dict[char.lower()] = 1    
 
+    odd_count = 0
+    for char in strng_dict:
+        if strng_dict[char.lower()] % 2 != 0:
+            if odd_count >= 1:
+                return False
+            odd_count += 1
 
+    return True
+
+# runtime is O(n) where n = # of characters in the string
 
 
 if __name__ == "__main__":
-    print "nothing yet"
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "tests pass"
+
