e93b7346f05bcd26365e4e3ded4480e5904b0e0c
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-10 13:49:07 -0700

    completed 1.5

--- chapter-1-arrays-and-strings/1.5-one-away.py
+++ chapter-1-arrays-and-strings/1.5-one-away.py
@@ -1,24 +1,53 @@
 """ 1.5 One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away. """
 
-def are_one_edit_away(str1, str2):
+
+def one_edit_away(s1, s2):
     """ Example inputs and outputs
 
-        >>> are_one_edit_away("pale", "ple")
+        >>> one_edit_away("pale", "ple")
         True
 
-        >>> are_one_edit_away("pales, pale")
+        >>> one_edit_away("pales", "pale")
         True
 
-        >>> are_one_edit_away("pale, bale")
+        >>> one_edit_away("pale", "bale")
         True
 
-        >>> are_one_edit_away("pale, bake")
+        >>> one_edit_away("pale", "bake")
         False
 
-        """
+    """
+    if abs(len(s1) - len(s2)) > 1:
+        return False
+    if len(s1) >= len(s2):
+        longer = s1
+        shorter = s2
+    else:
+        longer = s2
+        shorter = s1
+    edit = False
+    l = s = 0
+    while l < len(longer) and s < len(shorter):
+        if longer[l] != shorter[s]:
+            if edit:
+                return False
+            edit = True
+            if len(longer) == len(shorter):
+                s += 1
+                l += 1
+            else:
+                l += 1
+        else:
+            s += 1
+            l += 1
+    return True
 
 
 
 
 if __name__ == "__main__":
-    print "nothing yet"
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "tests pass"
+
+
