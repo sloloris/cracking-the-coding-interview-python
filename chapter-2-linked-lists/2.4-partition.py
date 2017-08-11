6f549728fbca9ef02b22361defe96ce0e56cdd93
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-11 15:57:16 -0700

    completed 2.4

--- chapter-2-linked-lists/2.4-partition.py
+++ chapter-2-linked-lists/2.4-partition.py
@@ -4,9 +4,36 @@ Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 with partition x=5
 Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 """
 
+from LL_implementation import LLNode, LinkedList
+
+def partition(ll, x):
+    partitioned = LinkedList()
+    current = ll.head
+    while current.next:
+        if current.data >= x:
+            partitioned.add(current.data)
+        else:
+            partitioned.insert_at_head(current.data)
+        current = current.next
+    if current.data >= x:
+        partitioned.add(current.data)
+    else:
+        partitioned.insert_at_head(current.data)
+    return partitioned
 
 
 
 
 if __name__ == "__main__":
-    print "nothing yet"
+    ll = LinkedList()
+    ll.add(3)
+    ll.add(5)
+    ll.add(8)
+    ll.add(5)
+    ll.add(10)
+    ll.add(2)
+    ll.add(1)
+
+    new_ll = partition(ll, 5)
+    new_ll._print() #remember that x itself can be anywhere in the second half of the output ll
+
