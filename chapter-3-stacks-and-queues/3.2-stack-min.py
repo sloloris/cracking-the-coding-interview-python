62a7aa0c604552602ccf4db064f7795ee8cbb905
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-14 16:48:58 -0700

    pseudo-completed 3.2

--- chapter-3-stacks-and-queues/3.2-stack-min.py
+++ chapter-3-stacks-and-queues/3.2-stack-min.py
@@ -1,9 +1,4 @@
 """ 3.2 Stack Min: How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, pop and min should all operate in O(1) time. """
 
+# You would want to implement your Stack from a custom LinkedList class where each Node has a next_min property and the LinkedList has a min property so that, when a node that is the minimum is pushed from the stack, it would update the stack min. Take into account how you deal with doubles.
 
-
-
-
-
-if __name__ == "__main__":
-    print "nothing yet"
